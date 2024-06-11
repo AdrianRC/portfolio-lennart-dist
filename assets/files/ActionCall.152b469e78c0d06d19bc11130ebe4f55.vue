2df0664634081869618ec270ba5d96650717d5a5
Adrian RC <adrian@me.com>
2024-06-11 16:50:14 +0000

    bump

--- assets/files/ActionCall.152b469e78c0d06d19bc11130ebe4f55.vue
+++ assets/files/ActionCall.152b469e78c0d06d19bc11130ebe4f55.vue
@@ -11,11 +11,10 @@
         class="inline-block px-6 py-2 mx-auto mt-8 text-xl leading-normal transition duration-150 bg-white border-2 border-dark text-dark rounded-xl hover:bg-black hover:text-white"
       >
         <span><slot name="button-text"></slot></span>
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M8.70711 1.29289C8.31658 0.902369 7.68342 0.902369 7.29289 1.29289C6.90237 1.68342 6.90237 2.31658 7.29289 2.70711L10.5858 6H3C2.44772 6 2 6.44772 2 7C2 7.55228 2.44772 8 3 8H10.5858L7.29289 11.2929C6.90237 11.6834 6.90237 12.3166 7.29289 12.7071C7.68342 13.0976 8.31658 13.0976 8.70711 12.7071L13.7071 7.70711C14.0976 7.31658 14.0976 6.68342 13.7071 6.29289L8.70711 1.29289Z"/>
-</svg>
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline ml-2 fill-current">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M6.70711 0.292893C6.31658 -0.0976311 5.68342 -0.0976311 5.29289 0.292893C4.90237 0.683418 4.90237 1.31658 5.29289 1.70711L8.58579 5H1C0.447715 5 0 5.44772 0 6C0 6.55228 0.447715 7 1 7H8.58579L5.29289 10.2929C4.90237 10.6834 4.90237 11.3166 5.29289 11.7071C5.68342 12.0976 6.31658 12.0976 6.70711 11.7071L11.7071 6.70711C12.0976 6.31658 12.0976 5.68342 11.7071 5.29289L6.70711 0.292893Z"></path>
 
-        </svg>
+</svg>
       </g-link>
     </div>
   </div>
